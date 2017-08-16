903b2b2144b28965c448b16a5f98ce8bb5ba88d3
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-16 11:11:27 -0400

    Set up react app with starter data for initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">
+            Earthquakes!
+          </div>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
