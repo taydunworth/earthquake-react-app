9c9c2aa0676a737fa7a287aa3c689f18dba94daa
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-16 13:58:37 -0400

    Added additional styling

--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -4,7 +4,7 @@ class EarthquakeInfo extends Component {
   render() {
     return(
       <div className="earthquake-title">
-      This is a list of 8 Earthquakes occurring on the morning of July 14th across the United States.
+        <div className="text-box">This is a list of <strong>8 Earthquakes</strong> occurring on the morning of July 14th across the United States.</div>
       </div>
     )
   }
--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -10,9 +10,9 @@ class EarthquakeList extends Component {
           <div className="card">
             <div className="card-block">
               <h4 className="card-title">{earthquake.properties.title}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquake.mag}</h6>
-              <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquake.time).format('llll')}</h6>
-              <p className="card-text">Coordinates: {earthquake.coordinates}</p>
+              <h6 className="card-subtitle mb-2 text-muted"><strong>Magnitude:</strong> {earthquake.properties.mag}</h6>
+              <h6 className="card-subtitle mb-2 text-muted"><strong>Time:</strong> {moment(earthquake.time).format('llll')}</h6>
+              <p className="card-text"><strong>Coordinates:</strong> {earthquake.geometry.coordinates}</p>
 
               <a href={earthquake.properties.url} className="card-link">USGS Event Link</a>
 
