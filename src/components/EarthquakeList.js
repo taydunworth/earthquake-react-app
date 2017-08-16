39ca5abab450e5288514f8ddaa5eafdac6f8cc04
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-16 15:40:33 -0400

    Fetched data

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import earthquakes from '../data/earthquakes.js'
+
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
 
 class EarthquakeList extends Component {
+  state = {
+    features: []
+  }
+
+  componentDidMount() {
+    fetch(url).then(res => res.json()).then(({ features}) => {
+      this.setState({ features})
+    })
+  }
+
   render() {
-    let earthquake = earthquakes.features.map((earthquake) => {
+    let earthquakes = this.state.features.map(earthquake => {
       return (
         <div className="col-sm-6" key={earthquake.id}>
           <div className="card">
             <div className="card-block">
-              <h4 className="card-title">{earthquake.properties.title}</h4>
+              <h4 className="card-title">{earthquake.properties.place}</h4>
               <h6 className="card-subtitle mb-2 text-muted"><strong>Magnitude:</strong> {earthquake.properties.mag}</h6>
-              <h6 className="card-subtitle mb-2 text-muted"><strong>Time:</strong> {moment(earthquake.time).format('llll')}</h6>
+              <h6 className="card-subtitle mb-2 text-muted"><strong>Time:</strong> {moment(earthquake.properties.time).format('llll')}</h6>
               <p className="card-text"><strong>Coordinates:</strong> {earthquake.geometry.coordinates}</p>
-
               <a href={earthquake.properties.url} className="card-link">USGS Event Link</a>
-
             </div>
           </div>
         </div>
-      )
-    })
+        )
+      })
 
     return (
       <div className="quake-list">
         <div className="row">
-          {earthquake}
+          {earthquakes}
         </div>
       </div>
     )
   }
 }
-export default EarthquakeList
+
+export default EarthquakeList;
